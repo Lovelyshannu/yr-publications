558a090a04572d5fcdc41d42d3726fde0983c9ff
Lovelyshannu <vasamshettyshanmukh@gmail.com>
2025-06-02 17:13:19 +0530

    Add all project files

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Article = require('../models/article');
+const articles = await Article.find({ isApproved: true }).sort({ createdAt: -1 });
+
+res.render('articles', { articles, user: req.session.user });
 
 router.get('/documents', async (req, res) => {
   const articles = await Article.find({ isPublished: true });
